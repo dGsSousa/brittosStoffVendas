5584244c66b6dce8fc81ab93652dc173a468fef0
Douglas Sousa <dgsgithub@gmail.com>
2026-03-04 23:25:09 -0300

    inicio do projeto

---
+++ app/src/main/java/br/com/douglas/brittosstoffvendas/MainActivity.kt
@@ -0,0 +1,35 @@
+package br.com.douglas.brittosstoffvendas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import br.com.douglas.brittosstoffvendas.dataBinding.ActivityMainBinding
+
+class MainActivity: AppCompatActivity(){
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigation.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.loginFragment){
+                binding.bottomNavigation.visibility = View.GONE
+            } else {
+                binding.bottomNavigation.visibility = View.VISIBLE
+            }
+        }
+    }
+}
+
